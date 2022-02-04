cef34c99ce1637d73e628c9ddf0b9dd886efc37a
jeezy1052@gmail.com <jeezy1052@gmail.com>
2022-02-04 13:42:12 -0800

    hello

---
+++ src/components/ChannelListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ChannelList, useChatContext } from 'stream-chat-react'
+
+const ChannelListContainer = () => {
+  return (
+    <div>
+      ChannelListContainer
+    </div>
+  )
+}
+
+export default ChannelListContainer
